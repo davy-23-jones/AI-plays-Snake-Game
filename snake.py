44f15f7e62160b795bec6bf282146ed48ab4a600
davy23jones <dkshetty23032023@gmail.com>
2023-11-07 14:20:21 +0530

    Added reset function

--- snake.py
+++ snake.py
@@ -1,82 +1,167 @@
-import curses
-from random import randint
-#setup window
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
+import pygame
+import random
+from enum import Enum
+from collections import namedtuple
+
+pygame.init()
+font = pygame.font.Font('arial.ttf', 25)
+#font = pygame.font.SysFont('arial', 25)
 
 
+#reset
+#reward
+#play(action) ->direction
+#game iteration
+#is_collision
 
-#snake and food
-snake = [(4,10), (4,9), (4,8)]
-food = (10,20)
-
-
-#game logic
-score = 0
-
-ESC = 27
-key = curses.KEY_RIGHT
-
-while key!=ESC:
-    win.addstr(0, 2, 'SCORE'+str(score)+' ')
-    win.timeout(150 - (len(snake)) // 5 + len(snake)//10 % 120) #increase speed formula
-    prev_key = key
-    event = win.getch()
-    key = event if event!= -1 else prev_key
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+    UP = 3
+    DOWN = 4
     
-    if key not in [curses.KEY_LEFT, curses.KEY_RIGHT, curses.KEY_UP, curses.KEY_DOWN, ESC]:
-        key = prev_key
+Point = namedtuple('Point', 'x, y')
+
+# rgb colors
+WHITE = (255, 255, 255)
+RED = (200,0,0)
+BLUE1 = (0, 0, 255)
+BLUE2 = (0, 100, 255)
+BLACK = (0,0,0)
+
+BLOCK_SIZE = 20
+SPEED = 5
+
+class SnakeGameAI:
     
-    #calculate the next coordinates
-    y = snake[0][0]
-    x = snake[0][1]
-    if key == curses.KEY_DOWN:
-        y +=1
-    if key == curses.KEY_LEFT:
-        x -=1
-    if key == curses.KEY_UP:
-        y -=1
-    if key == curses.KEY_RIGHT:
-        x +=1    
-    event = win.getch()
+    def __init__(self, w=640, h=480):
+        self.w = w
+        self.h = h
+        # init display
+        self.display = pygame.display.set_mode((self.w, self.h))
+        pygame.display.set_caption('Snake')
+        self.clock = pygame.time.Clock()
+        
+        # init game state
+        self.direction = Direction.RIGHT
+        
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+
+    def reset(self):
+        # init game state
+        self.direction = Direction.RIGHT
+        
+        self.head = Point(self.w/2, self.h/2)
+        self.snake = [self.head, 
+                      Point(self.head.x-BLOCK_SIZE, self.head.y),
+                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+        
+        self.score = 0
+        self.food = None
+        self._place_food()
+        self.frame_iteration = 0
+
+
+    def _place_food(self):
+        x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
+        y = random.randint(0, (self.h-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE
+        self.food = Point(x, y)
+        if self.food in self.snake:
+            self._place_food()
+        
+    def play_step(self, action):
+        self.frame_iteration += 1
+        # 1. collect user input
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            
+        
+        # 2. move
+        self._move(action) # update the head
+        self.snake.insert(0, self.head)
+        
+        # 3. check if game over
+        reward = 0
+        game_over = False
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+            game_over = True
+            reward = -10
+            return reward, game_over, self.score
+            
+        # 4. place new food or just move
+        if self.head == self.food:
+            self.score += 1
+            reward = 10
+            self._place_food()
+        else:
+            self.snake.pop()
+        
+        # 5. update ui and clock
+        self._update_ui()
+        self.clock.tick(SPEED)
+        # 6. return game over and score
+        return reward, game_over, self.score
     
-    snake.insert(0, (y,x)) 
+    def _is_collision(self, pt=None):
+        if pt is None:
+            pt = self.head
+        # hits boundary
+        if pt.x > self.w - BLOCK_SIZE or pt.x < 0 or pt.y > self.h - BLOCK_SIZE or pt.y < 0:
+            return True
+        # hits itself
+        if self.head in self.snake[1:]:
+            return True
+        
+        return False
+        
+    def _update_ui(self):
+        self.display.fill(BLACK)
+        
+        for pt in self.snake:
+            pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            
+        pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
+        
+        text = font.render("Score: " + str(self.score), True, WHITE)
+        self.display.blit(text, [0, 0])
+        pygame.display.flip()
+        
+    def _move(self, action):
+        x = self.head.x
+        y = self.head.y
+        if direction == Direction.RIGHT:
+            x += BLOCK_SIZE
+        elif direction == Direction.LEFT:
+            x -= BLOCK_SIZE
+        elif direction == Direction.DOWN:
+            y += BLOCK_SIZE
+        elif direction == Direction.UP:
+            y -= BLOCK_SIZE
+            
+        self.head = Point(x, y)
+            
 
-    #check if we hit the border
-    if y == 0: break
-    if y == 19: break
-    if x == 0: break
-    if x == 59: break
-
-    # if snake runs over itself
-    if snake[0] in snake[1:]:
-        break
-
-    # if snake hit the food
-    if snake[0] == food:
-        #eat the food
-        score += 1
-        food = ()
-        while food == ():
-            food = (randint(1, 18), randint(1, 58))
-            if food in snake:
-                food = ()
-        win.addch(food[0], food[1], '#')
-    else:
-        #move snake
-        last = snake.pop()
-        win.addch(last[0], last[1], ' ')    
-
-    for c in snake:
-        win.addch(c[0], c[1], '*')
-    win.addch(food[0], food[1], '#')
-
-
-
-curses.endwin()
-print(f'Final score = {score}')
+if __name__ == '__main__':
+    game = SnakeGame()
+    
+    # game loop
+    while True:
+        game_over, score = game.play_step()
+        
+        if game_over == True:
+            break
+        
+    print('Final Score', score)
+        
+        
+    pygame.quit()
